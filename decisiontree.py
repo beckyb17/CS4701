e7efa15269897ea6e04a70325fc7e63d0148be63
Becky Borrazzo <rgb226@cornell.edu>
2021-04-26 12:17:21 -0400

    update dataset

--- decisiontree.py
+++ decisiontree.py
@@ -1,13 +1,9 @@
-class TreeNode {
-  int num;
-  TreeNode yes;
-  TreeNode no;
-
+class TreeNode:
   def __init__(self, num, yes, no):
     self.num = num
     self.yes = yes
     self.no = no
-}
+
 
 city_to_num = {0: "New York", 1:"Boston", 2:"Ithaca", 3:"Burlington", 4:"Austin",
 5:"Charlotte", 6:"San Francisco", 7:"Los Angeles", 8:"Seattle", 9:"Miami"}
@@ -15,7 +11,7 @@ city_to_num = {0: "New York", 1:"Boston", 2:"Ithaca", 3:"Burlington", 4:"Austin"
 questions_to_num = {1: "Would you like a big population?", 
 2:"Would you like a big population density?", 
 3:"Do you mind a high cost of living?", 4:"Are you working in finance?",
-5:"Are you working in tech?", 6:"Are you working in business?"
+5:"Are you working in tech?", 6:"Are you working in business?",
 7: "Are you working in medicine?", 8:"Are you working in education?", 9:"Are you working in entertainment?",
 10:"Are you working in music?", 11:"Do you like Italian food?", 12: "Do you like Mexican food?",
 13:"Do you like seafood?", 14:"Are you vegetarian?", 15:"Do you like fried food?", 
@@ -23,10 +19,10 @@ questions_to_num = {1: "Would you like a big population?",
 17:"Do you like watching sport games?", 18:"Do you like going to museums?", 19:"Do you like going to bars?",
 20: "Do you like hiking?", 21: "Do you like the beach?", 22:"Do you like shopping?",
 23:"Do you prefer to drive?", 24:"Do you prefer to walk?", 25:"Do you prefer to take the bus?",
-26:"Do you prefer to take the metro?"}
+26:"Do you prefer to take the metro?", 27:"Do you like to ski?"}
 
 industry_to_num = {0:"Finance", 1:"Tech", 2:"Business", 3:"Medicine", 4:"Education",
-5:"Entertainment", 6:"Music"}
+5:"Entertainment", 6:"Music", 6:"Skiing"}
 
 food_to_num = {0:"Italian", 1:"Mexican", 2:"Seafood", 3:"Vegetarian", 4:"Fried food", 5:"Barbecque"}
 
@@ -41,23 +37,29 @@ pop_density_to_num = {0:"Small", 1:"Big"}
 cost_living_to_num = {0:"Low", 1:"High"}
 
 question_to_index = {1:0,2:1,3:2,4:3,5:3,6:3,7:3,8:3,9:3,10:3,11:4,12:4,13:4,14:4,
-15:4,16:4,17:5,18:5, 19:5,20:5,21:5,22:5,23:6,24:6,25:6}
+15:4,16:4,17:5,18:5, 19:5,20:5,21:5,22:5,23:6,24:6,25:6,26:6, 27:5}
 
 question_to_yes_answers = {1:1,2:1,3:0,4:0,5:1,6:2,7:3,8:4,9:5,10:6,11:0,12:1,13:2,14:3,15:4,16:5,
-17:0,18:1,19:2,20:3,21:4,22:5,23:0,24:1,25:2,26:3}
+17:0,18:1,19:2,20:3,21:4,22:5,23:0,24:1,25:2,26:3, 27:6}
 
 #dataset takes shape of
 #size | pop density | cost of living | industry | food | activity | |transporation | label
-train_set = [[1,1,0,0,0,1,2,0], [1,1,0,1,0,2,1,0], [1,1,0,0,3,5,6,0], [1,1,0,2,0,2,3,0],
+train_set = [[1,1,0,0,0,1,2,0], [1,1,0,1,0,2,1,0], [1,1,0,3,5,6,1,0], [1,1,0,2,0,2,3,0],
 [1,1,0,3,0,0,3,1],[1,1,0,4,0,1,1,1],[1,1,0,1,2,3,3,1],[1,1,0,3,2,0,1,1],
-[0,0,0,4,3,3,2,2],[0,0,0,4,0,5,0,2],[0,0,0,3,3,3,0,2],
-[0,0,0,3,2,3,0,3],[0,0,0,4,0,5,2,3],[0,0,0,3,0,3,0,3]]
+[0,0,0,4,3,3,2,2],[0,0,0,4,0,5,0,2],[0,0,0,3,3,3,0,2],[0,0,0,3,4,6,2,2],
+[0,0,0,3,2,3,0,3],[0,0,0,4,0,5,2,3],[0,0,0,3,0,3,0,3],[0,0,0,4,2,5,2,3],
+[0,0,0,2,4,5,0,4],[0,0,0,1,2,0,0,4],[0,0,0,1,5,3,0,4],[0,0,0,4,4,2,2,4],
+[0,0,0,0,4,0,0,5],[0,0,0,0,5,3,0,5],[0,0,0,2,3,3,2,5],[0,0,0,3,3,5,0,5],
+[0,1,1,1,1,1,0,6],[0,1,1,1,2,4,3,6],[0,1,1,0,3,5,2,6],[0,1,1,0,3,1,1,6],
+[1,1,0,5,1,4,0,7],[1,1,0,5,3,2,2,7],[1,1,0,5,1,3,0,7],[1,1,0,6,1,5,0,7],
+[0,0,1,1,0,5,0,8],[0,0,1,1,3,1,1,8],[0,0,1,1,2,5,3,8],[0,0,1,2,0,3,3,8],
+[0,0,0,0,3,4,0,9],[0,0,0,2,3,4,2,9],[0,0,0,0,5,4,2,9],[0,0,0,2,0,5,0,9]]
 
 #given a question, breaks the set into "yes" answers and "no" answers
 def breakInputs(question_num, set_remaining):
   true = []
   false = []
-  index = question_to_index[question_num]:
+  index = question_to_index[question_num]
   for lst in set_remaining:
     answer = lst[index]
     if answer in question_to_yes_answers[question_num]:
@@ -119,11 +121,11 @@ def create_Tree(dataset, prev_info_gain, questions):
   all_same = True
   for i in dataset:
     #if first round,
-    if r = 0:
-      class_seen = dataset[i][-1]
+    if r == 0:
+      class_seen = i[-1]
       r = 1
     else:
-      if not class_seen == dataset[i][-1]:
+      if not class_seen == i[-1]:
         all_same = False
   if all_same:
     return TreeNode(class_seen, None, None) #leaf node where the number is the city
@@ -140,13 +142,36 @@ def create_Tree(dataset, prev_info_gain, questions):
     return TreeNode(most_common_city, None, None)
   
   #not base case
-  best_q, highest_ig, true_set, false_set = get_question(dataset, parent_infogain, questions)
+  best_q, highest_ig, true_set, false_set = get_question(prev_info_gain, dataset, questions)
   questions.remove(best_q)
   true_side = create_Tree(true_set, highest_ig, questions)
   false_side = create_Tree(false_set, highest_ig, questions)
 
   return TreeNode(best_q, true_side, false_side)
 
+def getResult(node):
+  if node.yes == None and node.no == None:
+    city_result = city_to_num(node.num)
+    return city_result
+  question_num = node.num
+  user_response = input(questions_to_num[question_num])
+  valid = False
+  while not valid:
+    if user_response.lower() != 'y' or user_response.lower() != 'n':
+      user_response = ("Please enter a valid answer. 'y' for yes and 'n' for no. >>")
+    else:
+      valid = True
+  if user_response.lower() == 'y':
+    getResult(node.yes)
+  else:
+    getResult(node.no)
+
 def main():
+  print("in main")
   questions = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26]
-  tree = createTree(dataset, 1, questions)
+  tree = create_Tree(train_set, 1, questions)
+  result = getResult(tree)
+  print(result)
+
+if __name__ == '__main__':
+  main()
