4f5e630b8eeeeeb4b539cb2861bced56f784f6d7
Becky Borrazzo <rgb226@cornell.edu>
2021-05-18 20:19:27 -0400

    cleaned up code

--- tswiftevaluation.py
+++ tswiftevaluation.py
@@ -201,7 +201,6 @@ def evaluateTopTen(cos_sim, set_matrix, cos_constant, set_constant, random_nums)
   Returns: int
   """
   avg_sims = []
-  city_avg_sims = []
   #loop through all of the cities
   j = 0
   while j < 10:
@@ -215,39 +214,31 @@ def evaluateTopTen(cos_sim, set_matrix, cos_constant, set_constant, random_nums)
       k = 0
       sims_dic = {}
       city_sims_dic = {}
-      #combine the cosine sim and jaccard sim and sort from highest to smallest
+      #combine both cosine sims and sort from highest to smallest
       while k < len(new_cos_sim):
         cosine_similarity = new_cos_sim[k]
-        jaccard_similarity = new_set_sim[k]
-        sims_dic[k] = cosine_similarity*cos_constant + jaccard_similarity*set_constant
-        city_sims_dic[k] = jaccard_similarity
+        city_similarity = new_set_sim[k]
+        sims_dic[k] = cosine_similarity*cos_constant + city_similarity*set_constant
         k += 1
       total_sims_sorted = sorted(sims_dic.items(), key = lambda pair:pair[1], reverse = True)
-      city_sims_sorted = sorted(city_sims_dic.items(), key = lambda pair:pair[1], reverse = True)
       avg_top_10 = 0
       count = 0
-      avg_top_10_city = 0
       #compute the average score of the top 10
       while count < 10:
         avg_top_10 += total_sims_sorted[count][1]
-        avg_top_10_city += city_sims_sorted[count][1]
         count += 1
       avg_sims.append(avg_top_10/10)
-      city_avg_sims.append(avg_top_10_city/10)
     j += 1
   avg_total = 0
-  city_avg_total = 0
   #compute the average of all of the scores
   for num in avg_sims:
     avg_total += num
-  for num2 in city_avg_sims:
-    city_avg_total += num2
 
-  return avg_total/len(avg_sims), city_avg_total/len(city_avg_sims)
+  return avg_total/len(avg_sims)
 
 
 if __name__ == '__main__':
-  """
+
   #call first if haven't stemmed the matrix before
   #stemming()
   #compute the effects of stemming
@@ -285,7 +276,8 @@ if __name__ == '__main__':
     i += 1
 
   #originally: looked at how the cosine similarity changed among the top 10
-  #songs (evaluate top ten later changed to measure jaccard similarity)
+  #songs (evaluate top ten later changed to evaluate the addition of 
+  #city-song similarity)
   cs_matrix = stemming(2, True)
   random_nums = []
   cs_shape = np.shape(cs_matrix)
@@ -297,8 +289,8 @@ if __name__ == '__main__':
   top_avg = evaluateTopTen(cs_matrix, random_nums)
   print(top_avg)
 
-  """
-  #compute the effects of adding the jaccard similarity
+
+  #compute the effects of adding the similarity between cities and songs
   cs_matrix = np.load('cosine_matrix.npy')
   city_matrix = np.load('city_matrix.npy')
   random_nums = []
@@ -310,15 +302,13 @@ if __name__ == '__main__':
     if not random_num in random_nums:
       random_nums.append(random_num)
   
-  #change the cosine sim and jaccard sim weights
+  #change the cosine sim weights
   cs_weight = 100
   set_weight = 0
   while set_weight <= 100:
     print("cs weight " + str(cs_weight))
     print("set weight " + str(set_weight))
-    top_avg, city_top_avg = evaluateTopTen(cs_matrix, city_matrix, cs_weight, set_weight, random_nums)
-    if set_weight == 100:
-      print("city top 10 average is " + str(city_top_avg))
+    top_avg = evaluateTopTen(cs_matrix, city_matrix, cs_weight, set_weight, random_nums)
     cs_weight -= 10
     set_weight += 10
     print("top 10 average is " + str(top_avg))
